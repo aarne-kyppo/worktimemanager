3303381c4d37e1df57b8a2809c2297b789054081
Aarne Kyppö <kyppoa@gmail.com>
2013-06-08 22:52:54 +0300

    installation script modified for new directory structure

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 user="$SUDO_USER"
 echo $user
-if [ -d "/usr/share/worktime" ]
+if [ -f "/usr/share/worktime/worktime.py" ]
 then
 rm  /usr/share/worktime/worktime.py
+mkdir /usr/share/worktime/src
+elif  [ -d "/usr/share/worktime/src"]
+then
+rm /usr/share/worktime/src/*
 else
-mkdir /usr/share/worktime
+mkdir /usr/share/worktime/src
 fi
-cp worktime/* /usr/share/worktime
+cp worktime/* /usr/share/worktime/src
 chown -R "$user" /usr/share/worktime
 if [ -f "/usr/bin/worktimemanager" ]
 then
